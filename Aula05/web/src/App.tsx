f49f01a2d3eccc19200c278c8354ea9ce8ee03dd
unknown <guilherme-9951@live.com>
2024-03-12 21:19:11 -0300

    adicionando coissas da aula de react

---
+++ Aula05/web/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+import Nome from './components/Nome';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Nome />
+    </div>
+  );
+}
+
+export default App;
---
+++ Aula05/web/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import '../styles/Header.css';
+
+export default function Header() {
+    return (
+        <div className='header'>
+            <h1 className='logo'>Papelaria</h1>
+            <ul className='opcoes'>
+                <li className='btn'>Botao</li>
+                <li className='btn'>Botao</li>
+                <li className='btn'>Botao</li>
+            </ul>
+        </div>
+    )
+} 
