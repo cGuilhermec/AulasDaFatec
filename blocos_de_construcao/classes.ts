ebb8b7a09c201f5b77f7550bc3d534e3bee4e3e5
unknown <guilherme-9951@live.com>
2024-03-13 22:42:59 -0300

    adicionando os comandos que nao tinham ido antes

---
+++ blocos_de_construcao/classes.ts
@@ -0,0 +1,19 @@
+/* Definindo classes 
+**
+** Autor: Fabrício G. M. de Carvalho
+*/
+
+class Cachorro{
+    raca: string;
+
+    constructor(raca: string){
+        this.raca = raca;
+    };
+
+    latir(): string{        
+        return "au au au";
+    };
+
+};
+
+export { Cachorro};
